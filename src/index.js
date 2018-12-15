eecf126cdd0183e9333b5095366666c618f46cb3
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-15 01:30:52 -0300

    Configured redux devtools. Added empty store. Added empty rootReducer.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from 'redux-starter-kit'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
+
+import App from './App';
+import './index.css';
+
+export const store = configureStore({
+  devTools: true,
+  reducer: rootReducer
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
