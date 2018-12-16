17fadcb30498bf5e80186f15da8889046753f989
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-16 16:06:54 -0300

    ClipForm and NewClip styling.

--- src/videos/components/ClipForm/ClipForm.js
+++ src/videos/components/ClipForm/ClipForm.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 
-const styles = () => ({
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
   container: {
+    ...theme.mixins.gutters(),
     width: '100%',
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'red',
+    padding: theme.spacing.unit * 2
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
   },
   textField: {
     width: '100%',
@@ -30,47 +40,61 @@ const ClipForm = ({
   onSubmit
 }) => (
   <form onSubmit={onSubmit} clipindex={clipIndex} videoindex={videoIndex}>
-    <Grid container>
-      <Grid item xs={12}>
-        <TextField
-          id="clipName"
-          label="Clip name"
-          className={classes.textField}
-          value={name}
-          onChange={onNameChange}
-          margin="normal"
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item>
-            <TextField
-              id="clipStart"
-              label="From"
-              className={classes.textField}
-              value={start}
-              onChange={onStartChange}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="clipEnd"
-              label="To"
-              className={classes.textField}
-              value={end}
-              onChange={onEndChange}
-              margin="normal"
-            />
+    <Grid container spacing={16}>
+      <Paper className={classes.paper}>
+        <Grid item xs={12}>
+          <TextField
+            id="clipName"
+            label="Clip name"
+            className={classes.textField}
+            value={name}
+            onChange={onNameChange}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid
+            alignItems="center"
+            container
+            justify="space-between"
+            spacing={16}
+          >
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="clipStart"
+                label="From"
+                className={classes.textField}
+                value={start}
+                onChange={onStartChange}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="clipEnd"
+                label="To"
+                className={classes.textField}
+                value={end}
+                onChange={onEndChange}
+                margin="normal"
+              />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Button color="secondary" onClick={onCancel}>
-        Cancel
-      </Button>
-      <Button color="primary" onClick={onSubmit}>
-        Save
-      </Button>
+        <Grid
+          container
+          direction="row"
+          justify="flex-end"
+          alignItems="flex-end"
+        >
+          <Button className={classes.button} color="secondary" onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button className={classes.button} color="primary" onClick={onSubmit}>
+            Save
+          </Button>
+        </Grid>
+      </Paper>
     </Grid>
   </form>
 );
--- src/videos/components/Main/Main.js
+++ src/videos/components/Main/Main.js
@@ -13,8 +13,7 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
     ...theme.mixins.gutters(),
-    minHeight: '100vh',
-    height: '100%',
+    height: '100vh',
     padding: theme.spacing.unit * 2
   },
   container: {
@@ -24,21 +23,36 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.primary
+  },
+  columnContainer: {
+    flexDirection: 'column',
+    [theme.breakpoints.up('md')]: {
+      height: '100%',
+    }
+  },
+  newClipContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [theme.breakpoints.down('sm')]: {
+      flexBasis: '100%',
+      marginTop: theme.spacing.unit * 2,
+    }
   }
 });
 
 const Videos = ({ classes, video }) => (
   <div className={classes.root}>
     <Grid className={classes.container} container spacing={24}>
-      <Grid item xs={12} md={6}>
-        <Grid container>
-          <Grid item xs={12}>
+      <Grid item xs={12} md={6} className={classes.columnContainer}>
+        <Grid container className={classes.columnContainer}>
+          <Grid item>
             <Paper className={classes.paper}>
               <VideoPlayer videoUrl={video.videoUrl} />
               <VideoInfo video={video} />
             </Paper>
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs className={classes.newClipContainer}>
             <NewClip />
           </Grid>
         </Grid>
