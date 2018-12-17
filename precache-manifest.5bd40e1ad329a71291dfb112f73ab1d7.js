ee9787ebab2b0bbc88b1df8aa3435a5aa2ebb00f
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-17 02:18:53 -0300

    Updates

---
+++ precache-manifest.5bd40e1ad329a71291dfb112f73ab1d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e354b971f4db066fce32",
+    "url": "/react_video_slicer/static/js/runtime~main.e354b971.js"
+  },
+  {
+    "revision": "b79a1b61e3b4ab9a104b",
+    "url": "/react_video_slicer/static/js/main.b79a1b61.chunk.js"
+  },
+  {
+    "revision": "c1e826290ff111cbc2a4",
+    "url": "/react_video_slicer/static/js/1.c1e82629.chunk.js"
+  },
+  {
+    "revision": "b79a1b61e3b4ab9a104b",
+    "url": "/react_video_slicer/static/css/main.20158ccf.chunk.css"
+  },
+  {
+    "revision": "0af0c5e4d65523313a914b8003390e91",
+    "url": "/react_video_slicer/index.html"
+  }
+];
