b4bd7d3c277946ec39c86f330c8ca511ca09c20b
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-17 00:29:09 -0300

    Removed unused import at EditItemDialog.

--- src/videos/components/EditItemDialog/EditItemDialog.js
+++ src/videos/components/EditItemDialog/EditItemDialog.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import ClipForm from 'videos/components/ClipForm';
