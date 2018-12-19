184b0767bbd0564c23a5e3a0dc73cfb1896424d9
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-19 15:41:36 -0300

    Tested filters reducer.

---
+++ src/filters/__tests__/reducer.test.js
@@ -0,0 +1,38 @@
+import filters from '../reducer';
+import { emptyFilters, setFilters } from '../actions';
+
+const initialState = {
+  nameFilter: 'nameFilter',
+  tagFilter: 'tagFilter'
+};
+
+describe('Filters / Reducer', () => {
+  let result, expectedNewState;
+
+  describe('case: EMPTY_FILTERS', () => {
+    beforeAll(() => {
+      result = filters(initialState, emptyFilters());
+      expectedNewState = { nameFilter: '', tagFilter: '' };
+    });
+
+    it('should turn all filters into empty strings', () =>
+      expect(result).toEqual(expectedNewState));
+  });
+
+  describe('case: SET_FILTERS', () => {
+    let newNameFilter, newTagFilter;
+
+    beforeAll(() => {
+      newNameFilter = 'new name filter';
+      newTagFilter = 'new tag filter';
+      result = filters(initialState, setFilters(newNameFilter, newTagFilter));
+      expectedNewState = {
+        nameFilter: newNameFilter,
+        tagFilter: newTagFilter
+      };
+    });
+
+    it('should set change filters to their new values', () =>
+      expect(result).toEqual(expectedNewState));
+  });
+});
