476aeef35e334a1d0bd8dd840140f5db15ee02ed
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-19 02:35:45 -0300

    Added comment in Videos initial state explaining why the 'videos' key is an Array.

--- src/videos/constants/initialState.js
+++ src/videos/constants/initialState.js
@@ -2,6 +2,8 @@ import { DEFAULT_VIDEO_URL } from './index';
 
 import { getClipsFromLocalStorage } from 'videos/utils';
 
+// videos state is an Array because the app was
+// originally conceived to handle multiple videos
 export default [
   {
     duration: 0,
