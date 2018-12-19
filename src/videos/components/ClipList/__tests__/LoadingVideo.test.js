9c4e01ea7a121e8e3cf7532c9fbec95428169b92
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-19 17:42:51 -0300

    Tested Videos / Components / ClipList / LoadingVideo.

---
+++ src/videos/components/ClipList/__tests__/LoadingVideo.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createMount } from '@material-ui/core/test-utils';
+
+import LoadingVideo from '../LoadingVideo';
+
+describe('Videos / Components / ClipList / LoadingVideo', () => {
+  let mount;
+  let loadingVideo, wrapper;
+
+  beforeAll(() => {
+    mount = createMount()
+    wrapper = mount(<LoadingVideo />);
+    loadingVideo = wrapper.find('LoadingVideo');
+    console.log(loadingVideo.debug());
+  });
+
+  it('should render', () =>
+    expect(wrapper.isEmptyRender()).toEqual(false));
+
+  it('should render a LoadingVideo component', () =>
+    expect(loadingVideo.isEmptyRender()).toEqual(false));
+
+  it('should render a descriptive text implying that the video is loading', () =>
+    expect(loadingVideo.text()).toEqual('Please wait...'));
+});
