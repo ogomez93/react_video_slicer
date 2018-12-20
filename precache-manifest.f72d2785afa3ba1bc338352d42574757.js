52d1d902d548a0612e655c021f15a83d61ccaa97
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-12-19 22:35:10 -0300

    Updates

--- precache-manifest.f72d2785afa3ba1bc338352d42574757.js
+++ precache-manifest.f72d2785afa3ba1bc338352d42574757.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react_video_slicer/static/js/runtime~main.e354b971.js"
   },
   {
-    "revision": "65b883ef8dfbdadae9e9",
-    "url": "/react_video_slicer/static/js/main.65b883ef.chunk.js"
+    "revision": "cb56c6a29358abc00fe9",
+    "url": "/react_video_slicer/static/js/main.cb56c6a2.chunk.js"
   },
   {
     "revision": "46d10baa06afa2be933e",
     "url": "/react_video_slicer/static/js/1.46d10baa.chunk.js"
   },
   {
-    "revision": "65b883ef8dfbdadae9e9",
+    "revision": "cb56c6a29358abc00fe9",
     "url": "/react_video_slicer/static/css/main.fddf4f78.chunk.css"
   },
   {
-    "revision": "3c29a367f7e962f39ed51eba2efaaee9",
+    "revision": "eff28a37c8b8fab377e4686393590322",
     "url": "/react_video_slicer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_video_slicer/precache-manifest.9fd33781edfea1df44eb61927fc93bc1.js"
+  "/react_video_slicer/precache-manifest.f72d2785afa3ba1bc338352d42574757.js"
 );
 
 workbox.clientsClaim();
